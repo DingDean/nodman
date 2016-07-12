0506a28a0f6824fca2707576d12ac41937fce1f0
DingDean <dingkework@hotmail.com>
2016-07-12 14:28:26 +0800

    Support Logging

---
+++ nodman.client.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var fork = require('child_process').fork;
+
+
+/* global variables */
+var config = require('./config.json');
+
+/* Express 服务器开启监听 */
+server.listen(4501, function () {
+    console.log('Client is hosting on port ' + 4501);
+});
+
+/* Setup Redis */
+var redis = require('redis');
+var subscriber = redis.createClient();
+var publisher = redis.createClient();
+
+subscriber.on('error', (err) => {
+    console.log('Error: ' + err);
+});
+subscriber.on('connect', () => {
+    console.log('subscriber connected!');
+});
+publisher.on('error', (err) => {
+    console.log('Error: ' + err);
+});
+publisher.on('connect', () => {
+    console.log('publiser connected!');
+});
+
+subscriber.subscribe(config.subscribe);
+
+// 此管理员管理的服务器
+var child_node = null;
+
+subscriber.on('message', (channel, msg) => {
+    if (channel != config.subscribe) return;
+    switch(msg)
+    {
+        case 'start': 
+            if (child_node) {
+                publish("Server is already running!");
+            } else {
+                child_node = forkNode(config.fork);
+                if (child_node) {
+                    publish('Server is running!');
+                }
+            }
+            break;
+        case 'close':
+            if (!child_node) {
+                publish("Server is already closed");
+            } else {
+                child_node.kill('SIGKILL');
+                child_node = null;
+                publish('Server is closed!');
+            }
+            break;
+        case 'restart':
+            if (child_node) {
+                child_node.kill('SIGKILL');
+                child_node = null;
+            }
+            child_node = forkNode(config.fork);
+            if (child_node) {
+                publish("Server is restarted!");
+            }
+            break;
+        case 'show log' : 
+            if (!child_node) {
+                return publish("Server is not started. Show log doesn't work!")
+            }
+            if (child_node.stdout) {
+                child_node.stdout.on('data', (data) => {
+                    publish(data);
+                });
+            } else {
+                publish("The output can not be obtained!");
+            }
+            break;
+        default: 
+            console.log(msg);
+    }
+});
+
+
+function publish (msg)
+{
+    publisher.publish(config.publish, msg);
+}
+
+function forkNode (config)
+{
+    var modulePath = config.modulePath ? config.modulePath : null;
+    var args = config.args ? config.args : [];
+    var options = config.options ? config.options : {};
+    if (!modulePath) return null;
+    return fork(modulePath, args, options);
+}
+
---
+++ nodman.master.js
@@ -0,0 +1,112 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+io.sockets.setMaxListeners(0);
+var spawn = require('child_process').fork;
+
+
+/* Express 静态文件指向设置 */
+app.use('/', express.static(__dirname));
+//app.use('/public', express.static(__dirname + '/public'));
+//app.use('/src', express.static(__dirname + '/src'));
+
+/* Express 路由设置 */
+app.route('/').get(function (req, res) {
+    console.log('incomming req');
+    res.sendFile(__dirname + '/index.html');
+});
+
+/* 设置Redis, 一个用来监听，一个用来发布 */
+var redis = require('redis');
+
+// 发布者
+var publisher = redis.createClient();
+publisher.on('error', (err) => {
+    console.log('Error: ' + err);
+});
+publisher.on('connect', () => {
+    console.log('publisher connected!');
+});
+
+//监听者
+var subscriber = redis.createClient();
+subscriber.on('error', (err) => {
+    console.log('Error: ' + err);
+});
+subscriber.on('connect', () => {
+    console.log('subscriber connected!');
+});
+
+var rooms = [];
+/* 监听main频道 */
+subscriber.subscribe('feedback');
+subscriber.on('message', (channel, msg) => {
+    if (channel!= 'feedback') {
+        return;
+    }
+    switch (msg) {
+        default:
+            console.log(msg);
+            rooms.reduce((pre, cur)=>{
+                io.to(cur).emit('logInfo', msg);
+                return true;
+            }, true);
+    }
+});
+
+
+/* 监听socket.io */
+io.on('connection', (socket) => {
+    var isJoinedRoom = false;
+    socket.on('start', (server) => {
+        publisher.publish(server, 'start');
+    });
+
+    socket.on('close', (server) => {
+        publisher.publish(server, 'close');
+    });
+
+    socket.on('restart', (server) => {
+        publisher.publish(server, 'restart');
+    });
+
+    socket.on('reload', (server) => {
+        publisher.publish(server, 'reload');
+    });
+
+    socket.on('show log', (server) => {
+        if (isJoinedRoom) {
+            console.log(`${socket.id} has joined the room`);
+            console.log(`Current rooms is ${rooms}`);
+            publisher.publish(server, `${socket.id} is already outputing log`);
+        } else {
+            console.log(`${socket.id} is joining the room`);
+            isJoinedRoom = true;
+            rooms.push(socket.id);
+            publisher.publish(server, `show log`);
+            console.log(`Current rooms is ${rooms}`);
+        }
+    });
+
+    socket.on('close log', (server) => {
+        if (isJoinedRoom) {
+            console.log(`${socket.id} is leaving the room`);
+            rooms = rooms.filter((id) => id !== socket.id);
+            console.log(`Current rooms is ${rooms}`);
+            isJoinedRoom = false;
+        } else {
+            console.log(`${socket.id} has already left the room`);
+            console.log(`Current rooms is ${rooms}`);
+        }
+    });
+
+
+});
+
+/* Express 服务器开启监听 */
+server.listen(4500, function () {
+    console.log('Server Manager is hosting on port ' + 4500);
+});
+
+
